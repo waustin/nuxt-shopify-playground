18288a652af27e6a1c59f8d403d8ee77b23ac3cd
Wade Austin <wade@few.io>
2021-09-16 15:41:43 -0500

    product stuff

--- components/Shopify/Product.vue
+++ components/Shopify/Product.vue
@@ -4,20 +4,25 @@
       
 
      <div class="column is-two-fifths media-col">
+       
         <img :key="selectedImage"
-             :src="product.images.edges[0].node.originalSrc"
+             :src="selectedImage.originalSrc"
              class="selected-image mb-4" />
-      
+
         <div class="image-gallery">
           <img
             v-for="image in product.images.edges"
             :key="image.node.id"
             :src="image.node.transformedSrc"
+            @click="selectedImage = image.node"
           />
         </div>
      </div>
      <div class="column info-col">
        <h2 class="is-size-2 mb-2">{{ product.title }}</h2>
+       <p class="description mb-2">
+            {{product.description}}
+        </p>
      </div>
 
     </div>
@@ -33,5 +38,23 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      selectedImage: null,
+      selectedVariant: null,
+    };
+  },
+  watch: {
+    product(val) {
+      console.log('product watch ', val);
+    } 
+  },
+  created() {
+    this.selectedImage = this.product.images.edges[0].node;
+    this.selectedVariant = this.product.variants.edges[0].node;
+    console.log(this.product.images);
+    console.log(this.selectedImage);
+  },
+  
 }
 </script>
